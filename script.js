b1c43ea58f2332b2ee5797faf6788317fee905a2
Jacqueline Tran <composercookie@gmail.com>
2016-03-29 22:58:32 -0700

    another attempt

--- script.js
+++
@@ -1,7 +0,0 @@
-{% assign base = site.baseurl | prepend: site.url %}
-
-SimpleJekyllSearch.init({
-  searchInput: document.getElementById('search-input'),
-  resultsContainer: document.getElementById('results-container'),
-  json: '{{ base }}/search.json',
-});
